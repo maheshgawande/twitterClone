9450bad45eb9c158bf43248369f2e54a20cc372b
maheshgawande <maheshsg297@gmail.com>
2019-10-07 15:47:39 +0530

    Changes

--- src/Components/mainComponents/mainSection/SectionHeader.jsx
+++ src/Components/mainComponents/mainSection/SectionHeader.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const SectionHeader = ({ name, logo, username }) => {
+const SectionHeader = ({ heading, logo, subText }) => {
   return (
     <div className="section-header">
       <div className="home-refresh">
-        <span>{name}</span>
-        <span className="username"> {username} </span>
+        <span>{heading}</span>
+        <span className="subText"> {subText} </span>
       </div>
       {logo}
     </div>
